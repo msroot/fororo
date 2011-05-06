7b96a79e2a04ffb5b527d7d80ee7b0fc8d4c6a84
victornava <victornavav@gmail.com>
2011-05-06 21:33:33 +1000

    added toString Method

--- src/shared/Topic.java
+++ src/shared/Topic.java
@@ -1,5 +1,7 @@
 package shared;
 
+import java.util.*;
+
 public class Topic implements java.io.Serializable{
 	private String id = null;
 	private String name = null;
@@ -35,4 +37,20 @@ public class Topic implements java.io.Serializable{
 	public void userName(String userName) {this.userName = userName;}
 
 	public String created() {return created;}
+	
+	public String toString(){
+	    HashMap<String, String> map = new HashMap<String, String>();
+	    map.put("id", id);
+	    map.put("name", name);
+	    map.put("description", description);
+	    map.put("isActive", new Boolean(isActive).toString());
+	    map.put("userName", userName);
+	    map.put("created", created);
+	    return map.toString();
+	}
+	
+	public static void main(String[] args) {
+	   Topic t =  new Topic("1", "the name", "the description", true, "malaka", "now");
+       System.out.println(t);
+	}
 }
