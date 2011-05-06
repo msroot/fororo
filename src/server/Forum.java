bbee7e3870384b89001965597cc9ee26ad39cf90
victornava <victornavav@gmail.com>
2011-05-06 23:28:20 +1000

    testing forum.java

--- src/server/Forum.java
+++ src/server/Forum.java
@@ -11,157 +11,146 @@ import java.net.MalformedURLException;
 import java.util.*;
 
 public class Forum extends UnicastRemoteObject implements ForumInterface {
-    static final long serialVersionUID = 1; //to keep the compiler happy;
+	static final long serialVersionUID = 1; // to keep the compiler happy;
 
-    HashMap<String, User> users = null;
-    HashMap<String, ForumClientInterface> clients = null;
+	public HashMap<String, User> users = null;
+	public HashMap<String, ForumClientInterface> clients = null;
 
-    public Forum() throws RemoteException {
-        clients = new HashMap<String, ForumClientInterface>();
-        users = new HashMap<String, User>();
-    }
+	public Forum() throws RemoteException {
+		clients = new HashMap<String, ForumClientInterface>();
+		users = new HashMap<String, User>();
+	}
 
-    public User loginUser(String username, String password, ForumClientInterface client) throws RemoteException, ForumException {
+	public User loginUser(String username, String password,
+			ForumClientInterface client) throws RemoteException, ForumException {
 
-        User user = DBUser.getByName(username);
+		User user = DBUser.getByName(username);
 
-        if (user == null){ throw new ForumException("Invalid username or password"); }
+		if (user == null) {
+			throw new ForumException("Invalid username or password");
+		}
 
-        if (user.name().equals(username) && user.password().equals(password)){
-            users.put(user.name(), user);
-            clients.put(user.name(), client);
-            return user;
-        } else{
-            throw new ForumException("Invalid username or password");
-        }
-    }
+		if (user.name().equals(username) && user.password().equals(password)) {
+			users.put(user.name(), user);
+			clients.put(user.name(), client);
+			return user;
+		} else {
+			throw new ForumException("Invalid username or password");
+		}
+	}
 
-    public boolean logoutUser(String username) throws RemoteException {
-        if (!userIsLoggedIn(username)){ return false; }
-        users.remove(username);
-        clients.remove(username);
-        return true;
-    }
+	public boolean logoutUser(String username) throws RemoteException {
+		if (!userIsLoggedIn(username)) {
+			return false;
+		}
+		users.remove(username);
+		clients.remove(username);
+		return true;
+	}
 
-    // TOPICS
-    public List<Topic> getTopics() throws RemoteException { 	    	
-        return DBTopic.getAll();
-    }
-    
-    // TODO test
-    public Topic createTopic(User user, Topic topic) throws RemoteException{
-    	if(!userIsLoggedIn(user.name())){
-            throw new ForumException("User is not logged in");
-        }
-        
-    	User loggedUser = users.get(user.name());
+	// TOPICS
+	public List<Topic> getTopics() throws RemoteException {
+		return DBTopic.getAll();
+	}
 
-        if(loggedUser.type().equals(User.Type.ADMIN)){
-        	topic.isActive(true);
-        } else{
-        	topic.isActive(false);
-        }
-        
-        topic.userName(user.name());
-        
-        return DBTopic.create(topic);
-    }
+	public Topic createTopic(User user, Topic topic) throws RemoteException {
+		if (!userIsLoggedIn(user.name())) {
+			throw new ForumException("User is not logged in");
+		}
 
-    // TODO test
-    public Topic approveTopic(User user, Topic topic) throws RemoteException {
-        
-        if(!userIsLoggedIn(user.name())){
-            throw new ForumException("User is not logged in");
-        }
+		User loggedUser = users.get(user.name());
 
-        User loggedUser = users.get(user.name());
+		if (loggedUser.type().equals(User.Type.ADMIN)) {
+			topic.isActive(true);
+		} else {
+			topic.isActive(false);
+		}
 
-        if(!loggedUser.type().equals(User.Type.ADMIN)){
-            throw new ForumException("User must be ADMIN");
-        }
+		topic.userName(user.name());
 
-        if(DBTopic.getById(topic.id()) == null){
-            throw new ForumException("Topic not found");
-        }
-        
-        topic.isActive(true);
-        
-        return DBTopic.update(topic);
-    }
+		return DBTopic.create(topic);
+	}
 
-    // THREADS
-    public ForumThread getThreadById(String threadId) throws RemoteException{
-        return DBForumThread.getById(threadId);
-    }
+	public Topic approveTopic(User user, Topic topic) throws RemoteException {
 
-    public List<ForumThread> getThreadsByTopic(String topicId) throws RemoteException {
-        return DBForumThread.getAllByTopic(topicId);
-    }
+		if (!userIsLoggedIn(user.name())) {
+			throw new ForumException("User is not logged in");
+		}
 
-    //TODO Test
-    public ForumThread createThread(User user, ForumThread thread) throws RemoteException {
+		User loggedUser = users.get(user.name());
 
-        User loggedUser = users.get(user.name());
+		if (!loggedUser.type().equals(User.Type.ADMIN)) {
+			throw new ForumException("User must be ADMIN");
+		}
 
-        if (loggedUser != null && loggedUser.isActive()){
-            return DBForumThread.create(thread);
-        } else{
-            throw new ForumException("User:" + user.name() +"is not logged in or is inactive"); 
-        }
-    }
+		if (DBTopic.getById(topic.id()) == null) {
+			throw new ForumException("Topic not found");
+		}
 
+		topic.isActive(true);
 
-    public void throwForumException() throws ForumException{
-        throw new ForumException("giveMeAForumException()");
-    }
+		return DBTopic.update(topic);
+	}
 
-    private boolean userIsLoggedIn(String username){
-        return users.containsKey(username);
-    }
-    
-    public void init(){
-    	System.setSecurityManager(new RMISecurityManager());
-        try {
-            Naming.rebind("rmi://localhost:1099/Forum", this);
-            System.out.println("Created and registered Forum object");
-        }
-        catch (RemoteException re){ 
-            System.out.println("Unable to add remote object to registry");
-        }
-        catch (MalformedURLException me){          
-            System.out.println("Incorrect address for registry");
-        }
-        finally{
-            System.out.println("Now waiting for remote invocations");
-        }
-    }
-    
-    public static void main(String args[]){
-        try {
-            Forum server = new Forum();
-            server.getTopics();
-            // server.init();
-        }
-        catch (RemoteException re){ 
-            System.out.println("Unable to add remote object to registry");
-        }
+	// THREADS
+	public ForumThread getThreadById(String threadId) throws RemoteException {
+		return DBForumThread.getById(threadId);
+	}
 
+	public List<ForumThread> getThreadsByTopic(String topicId)
+			throws RemoteException {
+		return DBForumThread.getAllByTopic(topicId);
+	}
 
-//        System.setSecurityManager(new RMISecurityManager());
-//        try {
-//            Forum server = new Forum();
-//            Naming.rebind("rmi://localhost:1099/Forum", server);
-//            System.out.println("Created and registered Forum object");
-//        }
-//        catch (RemoteException re){ 
-//            System.out.println("Unable to add remote object to registry");
-//        }
-//        catch (MalformedURLException me){          
-//            System.out.println("Incorrect address for registry");
-//        }
-//        finally{
-//            System.out.println("Now waiting for remote invocations");
-//        }
-    	
-    }
+	// TODO Test
+	public ForumThread createThread(User user, ForumThread thread)
+			throws RemoteException {
+
+		User loggedUser = users.get(user.name());
+
+		if (loggedUser != null && loggedUser.isActive()) {
+			return DBForumThread.create(thread);
+		} else {
+			throw new ForumException("User:" + user.name()
+					+ "is not logged in or is inactive");
+		}
+	}
+
+	public void throwForumException() throws ForumException {
+		throw new ForumException("giveMeAForumException()");
+	}
+
+	private boolean userIsLoggedIn(String username) {
+		return users.containsKey(username);
+	}
+
+	public void init() {
+		System.setSecurityManager(new RMISecurityManager());
+		try {
+			Naming.rebind("rmi://localhost:1099/Forum", this);
+			System.out.println("Created and registered Forum object");
+		} catch (RemoteException re) {
+			System.out.println("Unable to add remote object to registry");
+		} catch (MalformedURLException me) {
+			System.out.println("Incorrect address for registry");
+		} finally {
+			System.out.println("Now waiting for remote invocations");
+		}
+	}
+
+	public static void main(String args[]) {
+		System.setSecurityManager(new RMISecurityManager());
+		try {
+			Forum server = new Forum();
+			Naming.rebind("rmi://localhost:1099/Forum", server);
+			System.out.println("Created and registered Forum object");
+		} catch (RemoteException re) {
+			System.out.println("Unable to add remote object to registry");
+		} catch (MalformedURLException me) {
+			System.out.println("Incorrect address for registry");
+		} finally {
+			System.out.println("Now waiting for remote invocations");
+		}
+
+	}
 }
--- src/test/ForumTest.java
+++ src/test/ForumTest.java
@@ -3,111 +3,193 @@ package test;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.rmi.RemoteException;
+import java.util.*;
 import server.*;
 import client.*;
 import shared.*;
 
 public class ForumTest {
-	
+
+	// @Test
+	// public void accepts_connections() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// } catch (Exception e){
+	// fail("Should not throw exception: " + e.getStackTrace());
+	// }
+	// }
+	//
+	// @Test
+	// public void user_can_login() {
+	// try{
+	// ForumClient client = new ForumClient();
+	//
+	// assertTrue("It should connect", client.connect());
+	//
+	// User user = client.forum.loginUser("testuser", "abcd1234", client);
+	//
+	// assertTrue("it shuold login => testuser:abcd1234", user instanceof User
+	// );
+	// assertTrue("username shuold be: testuser but is: " +user.name(),
+	// "testuser".equals(user.name()));
+	// assertTrue("password shuold be: abcd1234 but is: " +user.password(),
+	// "abcd1234".equals(user.password()));
+	// } catch (Exception e){
+	// fail("Should not throw exception: " + e.getStackTrace());
+	// }
+	// }
+	//
+	// @Test
+	// public void user_can_logout() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// User user = client.forum.loginUser("testuser", "abcd1234", client);
+	// assertTrue(client.forum.logoutUser(user.name()));
+	// } catch (Exception e){
+	// fail("Should not throw exception: " + e.getMessage());
+	// }
+	// }
+	//
+	// @Test
+	// public void can_get_topic_list() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// client.forum.getTopics();
+	// } catch (Exception e){
+	// e.printStackTrace();
+	// fail("Should not throw exception: " + e.getMessage());
+	// }
+	// }
+	//
+	// @Test
+	// public void can_get_threads_by_topic() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// //TODO check threads are correct
+	// client.forum.getThreadsByTopic("0");
+	// } catch (Exception e){
+	// e.printStackTrace();
+	// fail("Should not throw exception: " + e.getMessage());
+	// }
+	// }
+	//
+	// @Test
+	// public void can_get_thread_by_id() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// //TODO check if thread exists in db
+	// client.forum.getThreadById("0");
+	// } catch (Exception e){
+	// e.printStackTrace();
+	// fail("Should not throw exception: " + e.getMessage());
+	// }
+	// }
+	//
+	// @Test
+	// public void can_create_thread() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// client.forum.createThread("0", "the title", "the description",
+	// "the_topic_id");
+	// } catch (Exception e){
+	// e.printStackTrace();
+	// fail("Should not throw exception: " + e.getMessage());
+	// }
+	// }
+	//
+	// @Test
+	// public void forum_exception_test() {
+	// try{
+	// ForumClient client = new ForumClient();
+	// assertTrue("It should connect", client.connect());
+	// client.forum.throwForumException();
+	// fail("it should throw a ForumException");
+	// } catch (RemoteException e){
+	// System.out.println("Catched RemoteException");
+	// System.out.println(e.getMessage());
+	// }
+	// }
+
 	@Test
-    public void accepts_connections() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-		} catch (Exception e){
-			fail("Should not throw exception: " + e.getStackTrace());
-		}
-	}
-	
-	@Test
-    public void user_can_login() {
-		try{
-			ForumClient client = new ForumClient();
-			
-			assertTrue("It should connect", client.connect());
-			
-			User user = client.forum.loginUser("testuser", "abcd1234", client); 
-			
-			assertTrue("it shuold login => testuser:abcd1234", user instanceof User );
-			assertTrue("username shuold be: testuser but is: " +user.name(), "testuser".equals(user.name()));
-			assertTrue("password shuold be: abcd1234 but is: " +user.password(), "abcd1234".equals(user.password()));
-		} catch (Exception e){
-			fail("Should not throw exception: " + e.getStackTrace());
+	public void can_get_topics() {
+		try {
+			Forum forum = new Forum();
+			List<Topic> topics = forum.getTopics();
+			for (Topic topic : topics) {
+				// System.out.println(topic);
+			}
+		} catch (RemoteException e) {
+			fail("it shuoldn't throw exception: " + e.getMessage());
 		}
 	}
 
 	@Test
-    public void user_can_logout() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			User user = client.forum.loginUser("testuser", "abcd1234", client);
-			assertTrue(client.forum.logoutUser(user.name()));
+	public void can_create_topic() {
+		try {
+			Forum forum = new Forum();
+			User user = new User("vic", "abcd1234", User.Type.NORMAL, true, "");
+			forum.users.put(user.name(), user); // fake login
+			Topic topic = new Topic("", "test topic", "the description", true,
+					user.name(), "");
+			Topic newTopic = forum.createTopic(user, topic);
+			assertNotNull("it should not be null", newTopic);
+			assertTrue("new Topic name shuold be: " + topic.name() + ", but is: " + topic.name(),
+					topic.name().equals(newTopic.name()));
+		} catch (RemoteException e) {
+			fail("it shuoldn't throw exception: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void admin_user_can_approve_topic() {
+		try {
+			Forum forum = new Forum();
+			User user = new User("john", "abcd1234", User.Type.ADMIN, true, "");
+			forum.users.put(user.name(), user); // fake login
+			Topic topic = forum.createTopic(user, new Topic("", "approve me", "to be approved", false, user.name(), ""));
+			Topic aprovedTopic = forum.approveTopic(user, topic);
+			assertNotNull("topic shouldn't be null", aprovedTopic);
+			assertTrue("topic.isActive() should be: true, but is: "+aprovedTopic.isActive(),
+					aprovedTopic.isActive()); 
+		} catch (RemoteException e) {
+			fail("it shuoldn't throw exception: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void normal_user_can_not_approve_topic() {
+		try {
+			Forum forum = new Forum();
+			User user = new User("vic", "abcd1234", User.Type.NORMAL, true, "");
+			forum.users.put(user.name(), user); // fake login
+			Topic topic = forum.createTopic(user, new Topic("", "approve me", "to be approved", false, user.name(), ""));
+			forum.approveTopic(user, topic);
+			fail("it should throw a RemoteException");
 		} catch (Exception e){
-			fail("Should not throw exception: " + e.getMessage());
+			assertTrue("Exception shuold be: shared.ForumException but is: " + e.getClass(),
+					e instanceof ForumException);
+			assertTrue(e.getMessage().matches("User must be ADMIN"));
 		}
 	}
 
-	@Test
-    public void can_get_topic_list() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			client.forum.getTopics();
-		} catch (Exception e){
-			e.printStackTrace();
-			fail("Should not throw exception: " + e.getMessage());
-		}
-	}
-	
-	@Test
-    public void can_get_threads_by_topic() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			//TODO check threads are correct
-			client.forum.getThreadsByTopic("0");
-		} catch (Exception e){
-			e.printStackTrace();
-			fail("Should not throw exception: " + e.getMessage());
-		}
-	}
-	
-	@Test
-    public void can_get_thread_by_id() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			//TODO check if thread exists in db			
-			client.forum.getThreadById("0");
-		} catch (Exception e){
-			e.printStackTrace();
-			fail("Should not throw exception: " + e.getMessage());
-		}
-	}
-	
-	@Test
-    public void can_create_thread() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			client.forum.createThread("0", "the title", "the description", "the_topic_id");
-		} catch (Exception e){
-			e.printStackTrace();
-			fail("Should not throw exception: " + e.getMessage());
-		}
-	}
-	
-	@Test
-    public void forum_exception_test() {
-		try{
-			ForumClient client = new ForumClient();
-			assertTrue("It should connect", client.connect());
-			client.forum.throwForumException();
-			fail("it should throw a ForumException");    
-		} catch (RemoteException e){
-			System.out.println("Catched RemoteException");
-			System.out.println(e.getMessage());
-		}
-	}
+//	@Test
+//	public void user_can_login() {
+//		try {
+//			Forum forum = new Forum();
+//			User user = forum.loginUser("vic", "abcd1234", new ForumClient());
+//
+//			assertNotNull("it shuold login => vic:abcd1234", user);
+//			assertTrue("username shuold be: vic but is: " + user.name(), user
+//					.name().equals("vic"));
+//
+//		} catch (Exception e) {
+//			fail("Should not throw exception: " + e.getStackTrace());
+//		}
+//	}
 }
